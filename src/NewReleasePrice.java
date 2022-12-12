0d9b438ab635ef22ea5af0bcc54cb7b2836046dd
Trihanabi <57278269+Trihanabi@users.noreply.github.com>
2022-12-12 00:20:55 -0500

    finished 13nd step

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -6,4 +6,11 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getChargeFor(int daysRented) {
+        double amount = 0;
+        amount += daysRented * 3;
+        return amount;
+    }
 }
--- src/Price.java
+++ src/Price.java
@@ -3,4 +3,6 @@ package bad.robot.refactoring.chapter1;
 public abstract class Price {
     public abstract int getPriceCode();
 
+    public abstract double getChargeFor(int daysRented);
+
 }
